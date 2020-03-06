6f83593e62de2e79e8f1c0e4c9250cfd74bd3cb7
Yago Santos <yagossc@gmail.com>
2020-03-05 23:41:24 -0300

    Add first exercise

---
+++ ex1_dust_off_that_compiler/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Main function */
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+
+  // Print the distance
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
+}
